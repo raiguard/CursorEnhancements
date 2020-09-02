eb99943ea1fc67889c29103f191c9210c68b5897
raiguard <caleb.heuer@gmail.com>
2020-09-01 23:56:40 -0600

    Support removing downgrades

--- src/constants.lua
+++ src/constants.lua
@@ -2,19 +2,23 @@ local constants = {}
 
 constants.default_overrides = {
   ["base"] = {
-    -- logistic chests
-    ["logistic-chest-requester"] = "logistic-chest-buffer",
-    ["logistic-chest-buffer"] = "logistic-chest-storage",
-    ["logistic-chest-storage"] = "logistic-chest-passive-provider",
-    ["logistic-chest-passive-provider"] = "logistic-chest-active-provider",
-    ["logistic-chest-active-provider"] = "steel-chest",
+    -- electric poles
+    ["small-electric-pole"] = "medium-electric-pole",
+    ["medium-electric-pole"] = "big-electric-pole",
+    ["big-electric-pole"] = "substation",
     -- inserters
     ["burner-inserter"] = "inserter",
     ["inserter"] = "long-handed-inserter",
     ["long-handed-inserter"] = "fast-inserter",
     ["fast-inserter"] = "filter-inserter",
     ["filter-inserter"] = "stack-inserter",
-    ["stack-inserter"] = "stack-filter-inserter"
+    ["stack-inserter"] = "stack-filter-inserter",
+    -- logistic chests
+    ["logistic-chest-requester"] = "logistic-chest-buffer",
+    ["logistic-chest-buffer"] = "logistic-chest-storage",
+    ["logistic-chest-storage"] = "logistic-chest-passive-provider",
+    ["logistic-chest-passive-provider"] = "logistic-chest-active-provider",
+    ["logistic-chest-active-provider"] = "steel-chest"
   }
 }
 
--- src/scripts/util.lua
+++ src/scripts/util.lua
@@ -1,21 +1,43 @@
 local util = {}
 
+-- TODO rename "upgrade" and "downgrade" to "next" and "previous"
 function util.apply_overrides(data, overrides, item_prototypes)
   for name, upgrade in pairs(overrides) do
-    if item_prototypes[name] and item_prototypes[upgrade] then
+    if item_prototypes[name] and (upgrade and item_prototypes[upgrade] or true) then
+      local old_upgrade
       -- this entity
       local entity_data = data[name]
       if entity_data then
-        entity_data.upgrade = upgrade
-      else
+        old_upgrade = entity_data.upgrade
+        if upgrade then
+          entity_data.upgrade = upgrade
+        else
+          entity_data.upgrade = nil
+          if not entity_data.downgrade then
+            data[name] = nil
+          end
+        end
+      elseif upgrade then
         data[name] = {upgrade=upgrade}
       end
-      -- upgrade's downgrade
-      local upgrade_data = data[upgrade]
-      if upgrade_data then
-        upgrade_data.downgrade = name
-      else
-        data[upgrade] = {downgrade=name}
+      -- old upgrade's downgrade
+      if old_upgrade then
+        local old_upgrade_data = data[old_upgrade]
+        if old_upgrade_data then
+          old_upgrade_data.downgrade = nil
+          if not old_upgrade_data.upgrade then
+            data[old_upgrade] = nil
+          end
+        end
+      end
+      -- new upgrade's downgrade
+      if upgrade then
+        local upgrade_data = data[upgrade]
+        if upgrade_data then
+          upgrade_data.downgrade = name
+        elseif upgrade then
+          data[upgrade] = {downgrade=name}
+        end
       end
     end
   end
