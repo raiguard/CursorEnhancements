68d6182c974b2adefa85441679492c946941caf3
Caleb Heuer <ch@raiguard.me>
2023-01-02 21:23:33 -0700

    Add migration

---
+++ migrations.lua
@@ -0,0 +1,16 @@
+local migration = require("__flib__/migration")
+
+local migrations = {
+  ["2.0.0"] = function()
+    global.built_item = {}
+    global.last_item = {}
+  end
+}
+
+local function on_configuration_changed(e)
+  migration.on_config_changed(e, migrations)
+end
+
+return {
+  on_configuration_changed = on_configuration_changed
+}
