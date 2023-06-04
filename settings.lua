fe935d4a2a86c4ca760d6182e235743b866498f8
Alex Kumbar <Rabmuk@users.noreply.github.com>
2023-06-04 18:47:32 -0400

    Added setting for quick craft amount (#20)
    * added quick-craft-count
    
    * quick craft count is based on player setting
    
    * Minor fixes
    
    ---------
    
    Co-authored-by: Caleb Heuer <ch@raiguard.me>

--- settings.lua
+++ settings.lua
@@ -5,4 +5,12 @@ data:extend({
     setting_type = "runtime-per-user",
     default_value = true,
   },
+  {
+    type = "int-setting",
+    name = "cen-quick-craft-count",
+    setting_type = "runtime-per-user",
+    minimum_value = 1,
+    maximum_value = 200,
+    default_value = 5,
+  },
 })
