b7400f7dd31ee0b1f275962d2b67ec4489d113a4
Caleb Heuer <ch@raiguard.me>
2024-11-20 14:58:12 +0900

    Change default item group scroll hotkeys to shift+alt+scroll

--- data.lua
+++ data.lua
@@ -13,11 +13,11 @@ data:extend({
   {
     type = "custom-input",
     name = "cen-scroll-next",
-    key_sequence = "ALT + mouse-wheel-up",
+    key_sequence = "SHIFT + ALT + mouse-wheel-up",
   },
   {
     type = "custom-input",
     name = "cen-scroll-previous",
-    key_sequence = "ALT + mouse-wheel-down",
+    key_sequence = "SHIFT + ALT + mouse-wheel-down",
   },
 })
